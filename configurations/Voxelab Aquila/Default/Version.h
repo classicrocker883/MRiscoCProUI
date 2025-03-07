44be46448d7aa8768cd9cffa1768b70355d24dae
classicrocker883 <andrewleduc88@yahoo.com>
2025-03-07 06:48:01 -0500

    Update - version to March

--- configurations/Voxelab Aquila/Default/Version.h
+++ configurations/Voxelab Aquila/Default/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.1.3g-2"
+#define SHORT_BUILD_VERSION "2.1.3g-3"
 
 /**
  * Verbose version identifier which should contain a reference to the location
