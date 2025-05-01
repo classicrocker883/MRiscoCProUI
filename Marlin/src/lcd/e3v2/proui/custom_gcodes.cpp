00b08882e4ed140c04fbcd241b3d25d6965aa9e0
Andrew <18502096+classicrocker883@users.noreply.github.com>
2025-05-01 19:24:51 -0400

    Update - custom_gcodes.cpp
    remove `safe_delay`

--- Marlin/src/lcd/e3v2/proui/custom_gcodes.cpp
+++ Marlin/src/lcd/e3v2/proui/custom_gcodes.cpp
@@ -57,8 +57,6 @@ void CError() {
         HMI_value.Color.g = parser.seenval('G') ? parser.value_byte() : 0;
         HMI_value.Color.b = parser.seenval('B') ? parser.value_byte() : 0;
         DWIN_ApplyColor(E);
-        safe_delay(300);
-        DWIN_RedrawScreen();
         break;
     }
   }
