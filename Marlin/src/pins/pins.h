4805dd4787faba59a81ff5a39805b23bfad312d0
Andrew <andrewleduc88@yahoo.com>
2025-07-21 03:46:56 -0400

    Update - adjust C2 DDL

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -911,7 +911,7 @@
 //
 
 #elif MB(AQUILA_V101)
-  #include "hc32f4/pins_AQUILA_101.h"               // HC32F460                             env:HC32F460C_aquila_101
+  #include "hc32f4/pins_AQUILA_101.h"               // HC32F460                             env:HC32F460C_aquila_101 env:HC32F460C_aquila_C2
 #elif MB(CREALITY_ENDER2P_V24S4)
   #include "hc32f4/pins_CREALITY_ENDER2P_V24S4.h"   // HC32F460                             env:HC32F460C_e2p24s4
 
