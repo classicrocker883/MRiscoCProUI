be03467448c242926feef8ef605deeccdbdc3cef
Andrew <andrewleduc88@yahoo.com>
2025-10-04 00:03:19 -0400

    Update - 2025-October

--- configurations/Creality Ender/UBL/Version.h
+++ configurations/Creality Ender/UBL/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.1.3g-9"
+#define SHORT_BUILD_VERSION "2.1.3g-10"
 
 /**
  * Verbose version identifier which should contain a reference to the location
