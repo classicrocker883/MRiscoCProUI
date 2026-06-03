551c1300f7f72963078287c842991bbca235cd30
Andrew <andrewleduc88@yahoo.com>
2026-06-02 20:11:55 -0400

    Update - 2026-June

--- configurations/BTT_SKR-Mini-E3-V3/MM/Version.h
+++ configurations/BTT_SKR-Mini-E3-V3/MM/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.1.3h-5"
+#define SHORT_BUILD_VERSION "2.1.3h-6"
 
 /**
  * Verbose version identifier which should contain a reference to the location
