cf24af10ff41d6ed71dedce19098ccba3adab897
Andrew <andrewleduc88@yahoo.com>
2026-08-03 20:12:39 -0400

    Update - 2026-August

--- configurations/Creality Ender/BLT/Version.h
+++ configurations/Creality Ender/BLT/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.1.3h-7"
+#define SHORT_BUILD_VERSION "2.1.3h-8"
 
 /**
  * Verbose version identifier which should contain a reference to the location
